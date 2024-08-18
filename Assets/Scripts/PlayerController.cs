6940af935f92fed440e930dd9429a3307f78f5ba
rqhu2025 <rhustudy@gmail.com>
2024-08-17 18:20:38 -0700

    fix variable name

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -98,7 +98,7 @@ public class PlayerMovement : MonoBehaviour
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
         if (!currScalePoint) return;
-        Vector2 normal = other.GetContact(0).normal;
+        Vector2 normal = collision.GetContact(0).normal;
         if (Vector2.Angle(normal, transform.position - currScalePoint.transform.position) < 90) {
             canIn = false;
         } else {
