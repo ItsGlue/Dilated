002da0af66dfcd7cdbf0119369ee1409f7c8a5d6
rqhu2025 <rhustudy@gmail.com>
2024-08-19 19:54:48 -0700

    Jump fix + faster scaling

--- Assets/Scripts/JumpDetect.cs
+++ Assets/Scripts/JumpDetect.cs
@@ -15,7 +15,7 @@ public class JumpDetect : MonoBehaviour
     {
         
     }
-    void OnTriggerEnter2D(Collider2D other) {
+    void OnTriggerStay2D(Collider2D other) {
         if (other.gameObject.tag == "Ground") {
             SendMessageUpwards("AllowJump", true);
         }
